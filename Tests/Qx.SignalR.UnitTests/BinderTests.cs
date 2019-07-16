6bcd4c62c1f15cb5a71eacf7cf097d224e372b5c
Nick Darvey <nickdarvey@outlook.com.au>
2019-07-16 22:49:41 +1000

    Adds tests for synth args

--- Tests/Qx.SignalR.UnitTests/BinderTests.cs
+++ Tests/Qx.SignalR.UnitTests/BinderTests.cs
@@ -27,9 +27,9 @@ namespace Qx.SignalR.UnitTests
         [Fact]
         public void TryBindInvocations_should_append_synthetic_parameters_types()
         {
-            var syntheticParameters = new[] { Expression.Parameter(typeof(bool)) };
+            var syntheticParameters = new[] { Expression.Parameter(typeof(bool)), Expression.Parameter(typeof(bool)) };
             var param = Expression.Parameter(typeof(Func<int, int>)); // Our unbound param doesn't know about the synthetic params
-            Expression<Func<int, bool, int>> lambda = (i, _) => 0 + i; // Our implementation depends on (well, discards) the synthetic params
+            Expression<Func<int, bool, bool, int>> lambda = (i, _, __) => 0 + i; // Our implementation depends on (well, discards) the synthetic params
             var args = new[] { Expression.Constant(1) }; // Our runtime arguments (non-synthetic, our synthetic args would be supplied later)
             var lambdaBindings = new Dictionary<ParameterExpression, LambdaExpression> { { param, lambda } };
 
@@ -41,6 +41,40 @@ namespace Qx.SignalR.UnitTests
             Assert.Equal(lambdaBindings.Values, invocationBindings.Values.Select(create => create(args).Expression));
         }
 
-        // TODO: Test for multiple synthetic args, ordering of synthetic args
+        [Fact]
+        public void TryBindInvocations_should_return_error_when_synthetic_parameters_types_are_out_of_order()
+        {
+            var syntheticParameters = new[] { Expression.Parameter(typeof(bool)), Expression.Parameter(typeof(string)) }; // bool, string
+            var param = Expression.Parameter(typeof(Func<int, int>));
+            Expression<Func<int, string, bool, int>> lambda = (i, _, __) => 0 + i; // string, bool
+            var args = new[] { Expression.Constant(1) };
+            var lambdaBindings = new Dictionary<ParameterExpression, LambdaExpression> { { param, lambda } };
+
+            var isBound = SignalRBinders.TryBindInvocations(lambdaBindings, syntheticParameters, out var invocationBindings, out var errors);
+
+            Assert.False(isBound);
+            Assert.Null(invocationBindings);
+            Assert.NotNull(errors);
+            Assert.NotEmpty(errors);
+            // TODO: String constant errors
+        }
+
+        [Fact]
+        public void TryBindInvocations_should_error_when_synthetic_parameters_are_missing()
+        {
+            var syntheticParameters = new[] { Expression.Parameter(typeof(bool)) }; // one synthetic param
+            var param = Expression.Parameter(typeof(Func<int, int>));
+            Expression<Func<int, bool, string, int>> lambda = (i, _, __) => 0 + i; // two synthetic params
+            var args = new[] { Expression.Constant(1) };
+            var lambdaBindings = new Dictionary<ParameterExpression, LambdaExpression> { { param, lambda } };
+
+            var isBound = SignalRBinders.TryBindInvocations(lambdaBindings, syntheticParameters, out var invocationBindings, out var errors);
+
+            Assert.False(isBound);
+            Assert.Null(invocationBindings);
+            Assert.NotNull(errors);
+            Assert.NotEmpty(errors);
+            // TODO: String constant errors
+        }
     }
 }
